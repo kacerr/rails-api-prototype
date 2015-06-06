7d819c602f4c1c1200a40fe28bc53c163005de3e
Honza Novak <kacerr.cz@gmail.com>
2015-06-06 09:45:54 +0200

    Initial commit + essential gems for devel and test setup

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+
+    scope module: :v1 do
+      # We are going to list our resources here
+    end    
+
+  end
+
+end
