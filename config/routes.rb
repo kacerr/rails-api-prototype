72b0609dac04695fc23e1cc0ac0b335ad2593f17
Honza Novak <kacerr.cz@gmail.com>
2015-06-06 15:48:16 +0200

    Updates application controller to prevent CSRF exception from being raised

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     # We are going to list our resources here
 
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, :only => [:show]
     end
