b9ea5947923a02766f9c473a6b1fc99e31a7c62a
Honza Novak <kacerr.cz@gmail.com>
2015-06-06 21:00:58 +0200

    Adds the authenticate with token method to handle access to actions

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -4,8 +4,10 @@ class Authentication
   include Authenticable
 end
 
+
 describe Authenticable do
   let(:authentication) { Authentication.new }
+  subject { authentication }
 
   describe "#current_user" do
     before do
@@ -17,4 +19,22 @@ describe Authenticable do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
   end
+
+  describe "#authenticate_with_token" do
+    
+    before do
+      @user = FactoryGirl.create :user
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(response).to receive(:response_code).and_return(401)
+      allow(response).to receive(:body).and_return({"errors" => "Not authenticated"}.to_json)
+      allow(authentication).to receive(:response).and_return(response)
+    end
+
+    it "render a json error message" do
+      expect(json_response[:errors]).to eql "Not authenticated"
+    end
+
+    it {  should respond_with 401 }
+  end
+
 end
